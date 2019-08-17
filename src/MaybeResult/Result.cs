4dcdb486df090e168b01541e071aa71c3a11cf5a
Nikolas Rølland Hugsted <nikolas.hugsted@bekk.no>
2019-08-17 20:55:53 +0200

    Moved fallback, added filter and map

--- src/MaybeResult/Result.cs
+++ src/MaybeResult/Result.cs
@@ -3,26 +3,6 @@ using MaybeResult.Exceptions;
 
 namespace MaybeResult
 {
-    public static partial class Result
-    {
-        public static Result<T, TError> Ok<T, TError>(T value)
-        {
-            return new Result<T, TError>(value, hasValue: true);
-        }
-
-        public static Result<T, TError> Error<T, TError>(TError error)
-        {
-            return new Result<T, TError>(error: error);
-        }
-
-        public static Result<T, TError> Join<T, TError>(Result<Result<T, TError>, TError> result)
-        {
-            return result.IsOk
-                ? result.Value
-                : Result.Error<T, TError>(result.ErrorOrThrow);
-        }
-    }
-
     public struct Result<T, TError>
     {
         private readonly T value;
@@ -47,17 +27,53 @@ namespace MaybeResult
         public T ValueOrThrow => IsOk ? value : throw new ValueDoesNotExistException();
 
         public TError ErrorOrThrow => IsError ? error : throw new ErrorDoesNotExistException();
+    }
 
-        public T ValueOrFallback(T fallbackValue) => IsOk ? value : fallbackValue;
+    public static partial class Result
+    {
+        public static Result<T, TError> Ok<T, TError>(T value)
+        {
+            return new Result<T, TError>(value, hasValue: true);
+        }
 
-        public T ValueOrFallback(Func<T> fallbackValueFn) => IsOk ? value : fallbackValueFn();
+        public static Result<T, TError> Error<T, TError>(TError error)
+        {
+            return new Result<T, TError>(error: error);
+        }
 
-        public Result<T, TError> OrFallback(T fallbackValue) => Result.Ok<T, TError>(fallbackValue);
+        public static Result<T, TError> Join<T, TError>(this Result<Result<T, TError>, TError> result)
+        {
+            return result.IsOk
+                ? result.Value
+                : Result.Error<T, TError>(result.ErrorOrThrow);
+        }
 
-        public Result<T, TError> OrFallback(Func<T> fallbackValueFn) => Result.Ok<T, TError>(fallbackValueFn());
+        public static Result<T2, TError> Map<T1, T2, TError>(
+            this Result<T1, TError> result,
+            Func<T1, T2> fn)
+        {
+            return result.IsOk
+                ? Result.Ok<T2, TError>(fn(result.Value))
+                : Result.Error<T2, TError>(result.Error);
+        }
 
-        public Result<T, TError> OrFallback(Result<T, TError> fallbackResult) => fallbackResult;
+        public static Result<T, TError2> MapError<T, TError1, TError2>(
+            this Result<T, TError1> result,
+            Func<TError1, TError2> fn)
+        {
+            return result.IsOk
+                ? Result.Ok<T, TError2>(result.Value)
+                : Result.Error<T, TError2>(fn(result.Error));
+        }
 
-        public Result<T, TError> OrFallback(Func<Result<T, TError>> fallbackResultFn) => fallbackResultFn();
+        public static Result<T, TError> Filter<T, TError>(
+            this Result<T, TError> result,
+            TError error,
+            Func<T, Boolean> fn)
+        {
+            return result.IsOk && fn(result.Value)
+                ? result
+                : Result.Error<T, TError>(error);
+        }
     }
 }
