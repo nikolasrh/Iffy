a3a340ed84e7be6ccb754329014e213d31dbea6b
Nikolas Rølland Hugsted <nikolas.hugsted@bekk.no>
2019-08-17 11:41:23 +0200

    Added Bind3, Bind4, Bind5 for Maybe

--- src/MaybeResult/Maybe.Bind.cs
+++ src/MaybeResult/Maybe.Bind.cs
@@ -19,5 +19,35 @@ namespace MaybeResult
         {
             return Lift2(fn, maybe1, maybe2).Join();
         }
+
+        public static Maybe<T4> Bind3<T1, T2, T3, T4>(
+            Func<T1, T2, T3, Maybe<T4>> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3)
+        {
+            return Lift3(fn, maybe1, maybe2, maybe3).Join();
+        }
+
+        public static Maybe<T5> Bind4<T1, T2, T3, T4, T5>(
+            Func<T1, T2, T3, T4, Maybe<T5>> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3,
+            Maybe<T4> maybe4)
+        {
+            return Lift4(fn, maybe1, maybe2, maybe3, maybe4).Join();
+        }
+
+        public static Maybe<T6> Bind5<T1, T2, T3, T4, T5, T6>(
+            Func<T1, T2, T3, T4, T5, Maybe<T6>> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3,
+            Maybe<T4> maybe4,
+            Maybe<T5> maybe5)
+        {
+            return Lift5(fn, maybe1, maybe2, maybe3, maybe4, maybe5).Join();
+        }
     }
 }
