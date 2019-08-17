9123d049e4e08f96e53f297a370156b4a50db5be
Nikolas Rølland Hugsted <nikolas.hugsted@bekk.no>
2019-08-17 21:08:03 +0200

    Added Bind 3-5

--- src/MaybeResult/Result.Bind.cs
+++ src/MaybeResult/Result.Bind.cs
@@ -1,23 +1,52 @@
 using System;
-using MaybeResult.Extensions;
 
 namespace MaybeResult
 {
     public static partial class Result
     {
         public static Result<T2, TError> Bind<T1, T2, TError>(
-            Func<T1, Result<T2, TError>> fn,
-            Result<T1, TError> result)
+            this Result<T1, TError> result,
+            Func<T1, Result<T2, TError>> fn)
         {
-            return Map(fn, result).Join();
+            return result.Map(fn).Join();
         }
 
         public static Result<T3, TError> Bind2<T1, T2, T3, TError>(
-            Func<T1, T2, Result<T3, TError>> fn,
-            Result<T1, TError> result1,
-            Result<T2, TError> result2)
+            this Result<T1, TError> result1,
+            Result<T2, TError> result2,
+            Func<T1, T2, Result<T3, TError>> fn)
         {
-            return Lift2(fn, result1, result2).Join();
+            return Lift2(result1, result2, fn).Join();
+        }
+
+        public static Result<T4, TError> Bind3<T1, T2, T3, T4, TError>(
+            this Result<T1, TError> result1,
+            Result<T2, TError> result2,
+            Result<T3, TError> result3,
+            Func<T1, T2, T3, Result<T4, TError>> fn)
+        {
+            return Lift3(result1, result2, result3, fn).Join();
+        }
+
+        public static Result<T5, TError> Bind4<T1, T2, T3, T4, T5, TError>(
+            this Result<T1, TError> result1,
+            Result<T2, TError> result2,
+            Result<T3, TError> result3,
+            Result<T4, TError> result4,
+            Func<T1, T2, T3, T4, Result<T5, TError>> fn)
+        {
+            return Lift4(result1, result2, result3, result4, fn).Join();
+        }
+
+        public static Result<T6, TError> Bind5<T1, T2, T3, T4, T5, T6, TError>(
+            this Result<T1, TError> result1,
+            Result<T2, TError> result2,
+            Result<T3, TError> result3,
+            Result<T4, TError> result4,
+            Result<T5, TError> result5,
+            Func<T1, T2, T3, T4, T5, Result<T6, TError>> fn)
+        {
+            return Lift5(result1, result2, result3, result4, result5, fn).Join();
         }
     }
 }
