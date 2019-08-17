25539d3b7dde732888037e936a7525be9f26a8bb
Nikolas Rølland Hugsted <nikolas.hugsted@bekk.no>
2019-08-17 11:42:35 +0200

    Added Lift3, Lift4, Lift5 for Maybe

--- src/MaybeResult/Maybe.Map.cs
+++ src/MaybeResult/Maybe.Map.cs
@@ -25,5 +25,51 @@ namespace MaybeResult
 
             return Maybe.Some<T3>(fn(maybe1.Value, maybe2.Value));
         }
+
+        public static Maybe<T4> Lift3<T1, T2, T3, T4>(
+            Func<T1, T2, T3, T4> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3)
+        {
+            if (maybe1.IsNone || maybe2.IsNone || maybe3.IsNone)
+            {
+                return Maybe.None<T4>();
+            }
+
+            return Maybe.Some<T4>(fn(maybe1.Value, maybe2.Value, maybe3.Value));
+        }
+
+        public static Maybe<T5> Lift4<T1, T2, T3, T4, T5>(
+            Func<T1, T2, T3, T4, T5> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3,
+            Maybe<T4> maybe4)
+        {
+            if (maybe1.IsNone || maybe2.IsNone || maybe3.IsNone || maybe4.IsNone)
+            {
+                return Maybe.None<T5>();
+            }
+
+            return Maybe.Some<T5>(fn(maybe1.Value, maybe2.Value, maybe3.Value, maybe4.Value));
+        }
+
+        
+        public static Maybe<T6> Lift5<T1, T2, T3, T4, T5, T6>(
+            Func<T1, T2, T3, T4, T5, T6> fn,
+            Maybe<T1> maybe1,
+            Maybe<T2> maybe2,
+            Maybe<T3> maybe3,
+            Maybe<T4> maybe4,
+            Maybe<T5> maybe5)
+        {
+            if (maybe1.IsNone || maybe2.IsNone || maybe3.IsNone || maybe4.IsNone || maybe5.IsNone)
+            {
+                return Maybe.None<T6>();
+            }
+
+            return Maybe.Some<T6>(fn(maybe1.Value, maybe2.Value, maybe3.Value, maybe4.Value, maybe5.Value));
+        }
     }
 }
